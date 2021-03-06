ada96e0449123651e49c0bd329217a5ce67a004e
Hamza <hamzazaman500@gmail.com>
2021-03-07 00:00:42 +0500

    First Commit

---
+++ prac.js
@@ -0,0 +1,12 @@
+//IIFEs in ES6
+
+// you just have to define scope 
+
+{
+    let firstName = 'Hamza';
+    let lastName = 'Zaman';
+    
+console.log(firstName + ' '+ lastName);
+}
+
+// console.log(firstName + ' '+ lastName);
